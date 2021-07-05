6a0ea1212126f489e734e1bc8b16b9d423026292
James Kokou GAGLO <freemanpolys@gmail.com>
2021-07-05 20:41:10 +0200

    Add folders

--- pkg/app/generate.go
+++ pkg/app/generate.go
@@ -2,10 +2,20 @@ package app
 
 import (
 	"log"
+	"os"
 
 	"github.com/andiwork/andictl/configs"
 )
 
 func Generate() {
+	test := "test/"
 	log.Println("les configs ======", configs.App)
+	// create app folder structure
+	// folder configs
+	os.MkdirAll(test+"configs", os.ModePerm)
+	// folder docs/swagger-ui
+	os.MkdirAll(test+"docs/swagger-ui", os.ModePerm)
+	// folder pkg
+	os.MkdirAll(test+"pkg", os.ModePerm)
+	// generate
 }
