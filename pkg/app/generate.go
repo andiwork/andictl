6676297922ad3317574863fb874360e0ae03eb1f
James Kokou GAGLO <freemanpolys@gmail.com>
2021-07-21 08:42:17 +0200

    Improve app generation with goroutine

--- pkg/app/generate.go
+++ pkg/app/generate.go
@@ -3,10 +3,13 @@ package app
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/andiwork/andictl/configs"
 	"github.com/andiwork/andictl/utils"
 )
+
+var wg sync.WaitGroup
 
 func Generate() {
 
@@ -18,6 +21,23 @@ func Generate() {
 	// folder docs/swagger-ui
 	os.MkdirAll(configs.AppDir+"docs/swagger-ui", os.ModePerm)
 	fmt.Println("create ", "docs/swagger-ui")
+	// Download swagger ui files
+	if _, err := os.Stat(configs.AppDir + "docs/swagger-ui/dist"); os.IsNotExist(err) {
+		wg.Add(1)
+		go func() {
+			swagger, err := utils.DownloadFile("v3.51.1.tar.gz", "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v3.51.1.tar.gz")
+			if err == nil {
+				//untar
+				utils.ExecShellCommand("tar -xzf "+swagger+" -C /tmp", []string{}, false)
+				utils.ExecShellCommand(fmt.Sprintf("mv /tmp/swagger-ui-3.51.1/dist %s/docs/swagger-ui", configs.AppDir), []string{}, false)
+			} else {
+				fmt.Printf("Error ", err)
+				os.Exit(0)
+			}
+			wg.Done()
+		}()
+
+	}
 
 	// folder pkg/middleware
 	os.MkdirAll(configs.AppDir+"pkg/middleware", os.ModePerm)
@@ -25,62 +45,66 @@ func Generate() {
 
 	//defer ExecShellCommand("go", []string{"mod", "tidy"})
 	//Generate main.go
-	data, _ := mainGoTmpl.ReadFile("templates/main.go.gotmpl")
-	utils.ProcessTmplFiles(configs.AppDir, "main.go", data, configs.AppConfs, false)
+	wg.Add(1)
+	go func() {
+		data, _ := mainGoTmpl.ReadFile("templates/main.go.gotmpl")
+		utils.ProcessTmplFiles(configs.AppDir, "main.go", data, configs.AppConfs, false)
 
-	data, _ = gitignoreTmpl.ReadFile("templates/gitignore.gotmpl")
-	utils.ProcessTmplFiles(configs.AppDir, ".gitignore", data, configs.AppConfs, false)
+		data, _ = gitignoreTmpl.ReadFile("templates/gitignore.gotmpl")
+		utils.ProcessTmplFiles(configs.AppDir, ".gitignore", data, configs.AppConfs, false)
+		wg.Done()
+	}()
 
 	// Generate configs
 	// package files
 	confDir := configs.AppDir + "configs"
+	go func() {
+		wg.Add(1)
+		data, _ := appTmpl.ReadFile("templates/app.yaml.gotmpl")
+		utils.ProcessTmplFiles(confDir, "app.yaml", data, configs.AppConfs, false)
 
-	data, _ = appTmpl.ReadFile("templates/app.yaml.gotmpl")
-	utils.ProcessTmplFiles(confDir, "app.yaml", data, configs.AppConfs, false)
+		data, _ = prodTmpl.ReadFile("templates/prod.yaml.gotmpl")
+		utils.ProcessTmplFiles(confDir, "prod.yaml", data, configs.AppConfs, false)
+		wg.Done()
+	}()
+	go func() {
+		wg.Add(1)
+		data, _ := appGoTmpl.ReadFile("templates/app.go.gotmpl")
+		utils.ProcessTmplFiles(confDir, "app.go", data, configs.AppConfs, false)
 
-	data, _ = prodTmpl.ReadFile("templates/prod.yaml.gotmpl")
-	utils.ProcessTmplFiles(confDir, "prod.yaml", data, configs.AppConfs, false)
+		data, _ = gormGoTmpl.ReadFile("templates/gorm.go.gotmpl")
+		utils.ProcessTmplFiles(confDir, "gorm.go", data, configs.AppConfs, false)
+		wg.Done()
+	}()
+	go func() {
+		wg.Add(1)
+		data, _ := restfulGoTmpl.ReadFile("templates/restful.go.gotmpl")
+		utils.ProcessTmplFiles(confDir, "restful.go", data, configs.AppConfs, false)
 
-	data, _ = appGoTmpl.ReadFile("templates/app.go.gotmpl")
-	utils.ProcessTmplFiles(confDir, "app.go", data, configs.AppConfs, false)
-
-	data, _ = gormGoTmpl.ReadFile("templates/gorm.go.gotmpl")
-	utils.ProcessTmplFiles(confDir, "gorm.go", data, configs.AppConfs, false)
-
-	data, _ = restfulGoTmpl.ReadFile("templates/restful.go.gotmpl")
-	utils.ProcessTmplFiles(confDir, "restful.go", data, configs.AppConfs, false)
-
-	data, _ = swaggerGoTmpl.ReadFile("templates/swagger.go.gotmpl")
-	utils.ProcessTmplFiles(confDir, "swagger.go", data, configs.AppConfs, false)
-
-	data, _ = authzGoTmpl.ReadFile("templates/authz.go.gotmpl")
-	utils.ProcessTmplFiles(configs.AppDir+"pkg/middleware", "authz.go", data, nil, false)
+		data, _ = swaggerGoTmpl.ReadFile("templates/swagger.go.gotmpl")
+		utils.ProcessTmplFiles(confDir, "swagger.go", data, configs.AppConfs, false)
+		data, _ = authzGoTmpl.ReadFile("templates/authz.go.gotmpl")
+		utils.ProcessTmplFiles(configs.AppDir+"pkg/middleware", "authz.go", data, nil, false)
+		wg.Done()
+	}()
 
 	if configs.AppConfs.App.AuthType == "jwt" {
-		os.MkdirAll(configs.AppDir+"utils", os.ModePerm)
-		data, _ = jwtGoTmpl.ReadFile("templates/jwt.go.gotmpl")
-		utils.ProcessTmplFiles(configs.AppDir+"pkg/middleware", "jwt.go", data, nil, false)
-		data, _ = swaggerHelperGoTmpl.ReadFile("templates/swagger_helper.go.gotmpl")
-		utils.ProcessTmplFiles(configs.AppDir+"utils", "swagger_helper.go", data, nil, false)
-	}
+		go func() {
+			wg.Add(1)
+			os.MkdirAll(configs.AppDir+"utils", os.ModePerm)
+			data, _ := jwtGoTmpl.ReadFile("templates/jwt.go.gotmpl")
+			utils.ProcessTmplFiles(configs.AppDir+"pkg/middleware", "jwt.go", data, nil, false)
+			data, _ = swaggerHelperGoTmpl.ReadFile("templates/swagger_helper.go.gotmpl")
+			utils.ProcessTmplFiles(configs.AppDir+"utils", "swagger_helper.go", data, nil, false)
+			wg.Done()
+		}()
 
-	// Download swagger ui files
-	if _, err := os.Stat(configs.AppDir + "docs/swagger-ui/dist"); os.IsNotExist(err) {
-
-		swagger, err := utils.DownloadFile("v3.51.1.tar.gz", "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v3.51.1.tar.gz")
-		if err == nil {
-			//untar
-			utils.ExecShellCommand("tar -xzf "+swagger+" -C /tmp", []string{}, false)
-			utils.ExecShellCommand(fmt.Sprintf("mv /tmp/swagger-ui-3.51.1/dist %s/docs/swagger-ui", configs.AppDir), []string{}, false)
-		} else {
-			fmt.Printf("Error ", err)
-			os.Exit(0)
-		}
 	}
+	wg.Wait()
 	// initialiaze go module
 	os.Chdir(configs.AppDir)
 	if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
 		utils.ExecShellCommand("go", []string{"mod", "init", configs.AppConfs.App.Name}, false)
 	}
-	defer utils.ExecShellCommand("go", []string{"mod", "tidy"}, false)
+	utils.ExecShellCommand("go", []string{"mod", "tidy"}, false)
 }
